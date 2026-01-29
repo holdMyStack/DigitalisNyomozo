65205174b5e708019268fa8e0f2a40c2260ddd09
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:40:54 +0100

    AdatTar létrehozása

---
+++ DigitalisNyomozo/AdatTar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class AdatTar
+	{
+		List<Felhasznalo> felhasznalok;
+		List<Ugy> ugyek;
+		List<Szemely> szemelyek;
+		List<Bizonyitek> bizonyitekok;
+
+		public AdatTar()
+		{
+			felhasznalok = new List<Felhasznalo>();
+			ugyek = new List<Ugy>();
+			szemelyek = new List<Szemely>();
+			bizonyitekok = new List<Bizonyitek>();
+		}
+	}
+}
