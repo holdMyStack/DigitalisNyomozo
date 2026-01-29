2ff2e65bdb34244a4bfbed0d20ca9adfcaf87d2c
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:30:29 +0100

    Gyanusitott létrehozása

---
+++ DigitalisNyomozo/Gyanusitott.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class Gyanusitott
+	{
+		Szemely szemely;
+		int gyanusitottsagiSzint;
+		string statusz;
+
+		public Gyanusitott(Szemely szemely, int gyanusitottsagiSzint, string statusz)
+		{
+			this.szemely = szemely;
+			this.gyanusitottsagiSzint = gyanusitottsagiSzint;
+			this.statusz = statusz;
+		}
+	}
+}
