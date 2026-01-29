48404d47f66b89913b6ca5661d76d700d5e46d4d
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:32:40 +0100

    IdovonalEsemeny létrehozása

---
+++ DigitalisNyomozo/IdovonalEsemeny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class IdovonalEsemeny
+	{
+		DateTime datum;
+		string esemenyLeiras;
+
+		public IdovonalEsemeny(DateTime datum, string esemenyLeiras)
+		{
+			this.datum = datum;
+			this.esemenyLeiras = esemenyLeiras;
+		}
+	}
+}
