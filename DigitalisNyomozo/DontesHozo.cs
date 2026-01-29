4d6492607277d38fa4cd2f7d1ac0811892b16c4c
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:39:18 +0100

    DontesHozo létrehozása

---
+++ DigitalisNyomozo/DontesHozo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class DontesHozo
+	{
+		public void GyanusitasFrissitese(Gyanusitott gyanusitott)
+		{
+			// ...
+		}
+	}
+}
