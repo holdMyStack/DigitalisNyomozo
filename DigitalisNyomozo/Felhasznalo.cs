85e0f25e67e677a5a4dc49e5996b53f28662cf2b
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:25:52 +0100

    Felhasznalo létrehozása

---
+++ DigitalisNyomozo/Felhasznalo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class Felhasznalo
+	{
+		string nev;
+		string azonosito;
+		string szerepkor;
+
+		public Felhasznalo(string nev, string azonosito, string szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+	}
+}
