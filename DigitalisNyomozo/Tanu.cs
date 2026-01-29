5fbb25f092b7b97b61f284936ccb0399842b2445
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:32:06 +0100

    Tanu létrehozása

---
+++ DigitalisNyomozo/Tanu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class Tanu
+	{
+		Szemely szemely;
+		string vallomasSzovege;
+		DateTime vallomasDatuma;
+
+		public Tanu(Szemely szemely, string vallomasSzovege, DateTime vallomasDatuma)
+		{
+			this.szemely = szemely;
+			this.vallomasSzovege = vallomasSzovege;
+			this.vallomasDatuma = vallomasDatuma;
+		}
+	}
+}
