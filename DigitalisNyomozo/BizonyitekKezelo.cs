cfa6bfacb0ab2fe8340f390922926bf9c577418f
Fata Dávid <fata.david@petrik.hu>
2026-01-29 14:35:13 +0100

    BizonytekKezelo létrehozása

---
+++ DigitalisNyomozo/BizonyitekKezelo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozo
+{
+	internal class BizonyitekKezelo
+	{
+		List<Bizonyitek> bizonyitekok;
+
+		public BizonyitekKezelo()
+		{
+			bizonyitekok = new List<Bizonyitek>();
+		}
+
+		public void HozzaAd(Bizonyitek bizonyitek)
+		{
+			bizonyitekok.Add(bizonyitek);
+		}
+
+		public void Torol(Bizonyitek bizonyitek)
+		{
+			bizonyitekok.Remove(bizonyitek);
+		}
+	}
+}
