b1084627f6eb4c36c18bdc5b092d878a367c67b6
Fata Dávid <fata.david@petrik.hu>
2026-02-05 15:17:45 +0100

    gyanusítottak almenü kezdete

--- DigitalisNyomozo/Program.cs
+++ DigitalisNyomozo/Program.cs
@@ -87,14 +87,14 @@
 			Console.WriteLine($"======== BRFK KEZELŐFELÜLET > {alMenu} ========\n");
 		}
 
-		static void SzemélyzetAlMenu(AdatTar tar)
+		static void SzemélyekAlMenu(AdatTar tar)
 		{
 			string[] menuOpciok = { "Listázás", "Felvétel", "Törlés", "Visszalépés" };
 			int opcio;
 
 			do
 			{
-				Fejlec("Személyzet");
+				Fejlec("Személyek");
 				Console.WriteLine("Válasszon műveletet:");
 
 				opcio = ValasztasMenu(menuOpciok);
@@ -193,8 +193,244 @@
 
 		static void UgyekAlMenu(AdatTar tar)
 		{
+			string[] menuOpciok = { "Listázás", "Felvétel", "Törlés", "Személy hozzárendelése", "Gyanusított hozzárendelése", "Bizonyíték hozzárendelése", "Visszalépés" };
+			int opcio;
 
-			
+			do
+			{
+				Fejlec("Ügyek");
+				Console.WriteLine("Válasszon műveletet:");
+
+				opcio = ValasztasMenu(menuOpciok);
+				Console.WriteLine();
+
+				switch (opcio)
+				{
+					case 0:
+						Fejlec("Ügyek listázása");
+
+						foreach (Ugy ugy2 in tar.ugyek)
+						{
+							Console.WriteLine(ugy2.ToString() + "\n");
+						}
+
+						Console.ReadKey();
+						break;
+
+					case 1:
+						Fejlec("Ügy hozzáadása");
+
+						Console.Write("Ügy azonosítója: ");
+						string ugyAzonosito = Console.ReadLine();
+
+						Console.Write("Ügy címe: ");
+						string ugyCim = Console.ReadLine();
+
+						Console.Write("Ügy állapota: ");
+						string ugyAllapot = Console.ReadLine();
+
+						Console.Write("Ügy leírása: ");
+						string ugyLeiras = Console.ReadLine();
+
+						Ugy ugy = new Ugy(ugyAzonosito, ugyCim, ugyLeiras, ugyAllapot, new List<Szemely>(), new List<Bizonyitek>(), new List<Gyanusitott>());
+
+						Fejlec("Ögy hozzáadása");
+
+						Console.WriteLine(ugy.ToString());
+						Console.Write("\nHelyes? (i/n) ");
+
+						char key2 = Console.ReadKey().KeyChar;
+						Console.WriteLine("");
+
+						if (key2 == 'i')
+						{
+							tar.ugyek.Add(ugy);
+							Console.WriteLine("\nÜgy hozzáadása sikeres.");
+						}
+						else
+						{
+							Console.WriteLine("\nÖgy hozzáadása megszakítva.");
+						}
+
+						Console.ReadKey();
+
+						break;
+
+					case 2:
+						Fejlec("Ügy törlése");
+
+						List<string> ugyek = new List<string>();
+
+						foreach (Ugy ugy3 in tar.ugyek)
+						{
+							ugyek.Add($"{ugy3.azonosito} {ugy3.cim} ({ugy3.allapot})");
+						}
+
+						int valasztott = ValasztasLista(ugyek);
+
+						Fejlec("Ügy törlése");
+
+						Console.WriteLine($"{tar.ugyek[valasztott].ToString()}");
+						Console.Write("\nHelyes? (i/n) ");
+
+						char key = Console.ReadKey().KeyChar;
+						Console.WriteLine("");
+
+						if (key == 'i')
+						{
+							tar.ugyek.Remove(tar.ugyek[valasztott]);
+							Console.WriteLine("\nÜgy eltávolítása sikeres.");
+						}
+						else
+						{
+							Console.WriteLine("\nÜgy eltávolítása megszakítva.");
+						}
+
+						Console.ReadKey();
+						break;
+
+					case 3:
+						Fejlec("Személy hozzárendelése");
+
+						if (tar.ugyek.Count == 0)
+						{
+							Console.WriteLine("Nincsenek ügyek.");
+							Console.ReadKey();
+
+							continue;
+						}
+
+						if (tar.szemelyek.Count == 0)
+						{
+							Console.WriteLine("Nincs hozzárendelhető személy.");
+							Console.ReadKey();
+
+							continue;
+						}
+
+
+						List<string> szemelyek2 = new List<string>();
+						List<string> ugyek3 = new List<string>();
+
+						foreach (Szemely szemely in tar.szemelyek)
+						{
+							szemelyek2.Add($"{szemely.nev}, született {2026 - szemely.eletkor}");
+						}
+
+						foreach (Ugy ugy3 in tar.ugyek)
+						{
+							ugyek3.Add($"{ugy3.azonosito} {ugy3.cim} - {ugy3.allapot}");
+						}
+
+						int valasztasSzemely2 = ValasztasLista(szemelyek2);
+
+						Fejlec("Személy hozzárendelése");
+
+						int valasztasUgy2 = ValasztasLista(ugyek3);
+
+						Fejlec("Személy hozzárendelése");
+
+						Console.WriteLine(tar.ugyek[valasztasUgy2].ToString() + "\n");
+						Console.WriteLine(tar.szemelyek[valasztasSzemely2].ToString());
+						Console.Write("\nHelyes? (i/n) ");
+
+						char key5 = Console.ReadKey().KeyChar;
+						Console.WriteLine("");
+
+						if (key5 == 'i')
+						{
+							tar.ugyek[valasztasUgy2].szemelyek.Add(tar.szemelyek[valasztasSzemely2]);
+
+							Console.WriteLine("\nSzemély hozzárendelése sikeres.");
+						}
+						else
+						{
+							Console.WriteLine("\nSzemély hozzárendelése megszakítva.");
+						}
+
+						Console.ReadKey();
+						break;
+
+					case 4:
+						Fejlec("Gyanusított hozzárendelése");
+
+						if (tar.ugyek.Count == 0)
+						{
+							Console.WriteLine("Nincsenek ügyek.");
+							Console.ReadKey();
+
+							continue;
+						}
+
+						if (tar.szemelyek.Count == 0)
+						{
+							Console.WriteLine("Nincs hozzárendelhető személy.");
+							Console.ReadKey();
+
+							continue;
+						}
+
+						List<string> szemelyek = new List<string>();
+						List<string> ugyek2 = new List<string>();
+
+						foreach (Szemely szemely in tar.szemelyek)
+						{
+							szemelyek.Add($"{szemely.nev}, született {2026 - szemely.eletkor}");
+						}
+
+						foreach (Ugy ugy2 in tar.ugyek)
+						{
+							ugyek2.Add($"{ugy2.azonosito} {ugy2.cim} - {ugy2.allapot}");
+						}
+
+						int valasztasSzemely = ValasztasLista(szemelyek);
+
+						Fejlec("Gyanusított hozzárendelése");
+
+						int valasztasUgy = ValasztasLista(ugyek2);
+
+						Fejlec("Gyanusított hozzárendelése");
+
+						int gyanusitottsag;
+						Console.Write("Gyanusítottsági szint (0-100): ");
+						bool sikeres = int.TryParse(Console.ReadLine(), out gyanusitottsag);
+
+						if (sikeres && gyanusitottsag > 0 && gyanusitottsag <= 100)
+						{
+							Console.Write("Gyanusított státusza: ");
+							string statusz = Console.ReadLine();
+
+							Gyanusitott gyanusitott = new Gyanusitott(tar.szemelyek[valasztasSzemely], gyanusitottsag, statusz);
+
+							Fejlec("Gyanusított hozzárendelése");
+
+							Console.WriteLine(tar.ugyek[valasztasUgy].ToString() + "\n");
+							Console.WriteLine(gyanusitott.ToString());
+							Console.Write("\nHelyes? (i/n) ");
+
+							char key4 = Console.ReadKey().KeyChar;
+							Console.WriteLine("");
+
+							if (key4 == 'i')
+							{
+								tar.ugyek[valasztasUgy].gyanusitottak.Add(gyanusitott);
+
+								Console.WriteLine("\nGyanusított hozzárendelése sikeres.");
+							}
+							else
+							{
+								Console.WriteLine("\nGyanusított hozzárendelése megszakítva.");
+							}
+						}
+						else
+						{
+							Console.WriteLine("\nÉrvénytelen érték, gyanusított hozzárendelése megszakítva.");
+						}
+
+						Console.ReadKey();
+						break;
+				}
+			} while (opcio != 6);
 		}
 
 		static void FoMenu()
@@ -204,7 +440,7 @@
 			UgyKezelo ugyKezelo = new UgyKezelo(tar);
 			BizonyitekKezelo bizonyitekKezelo = new BizonyitekKezelo(tar);
 
-			string[] menuOpciok = { "Személyzet", "Ügyek", "Gyanusítottak", "Tanúk", "Bizonyítékok", "Kilépés" };
+			string[] menuOpciok = { "Személyek", "Ügyek", "Bizonyítékok", "Kilépés" };
 			int opcio;
 
 			do
@@ -217,7 +453,7 @@
 				switch (opcio)
 				{
 					case 0:
-						SzemélyzetAlMenu(tar);
+						SzemélyekAlMenu(tar);
 						break;
 
 					case 1:
